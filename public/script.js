fd1a4307eb874f0663ab5a49e125c5e81af62830
steven <hhuang@ban-qu.com>
2023-10-28 02:59:11 +0800

    add user video stream

--- public/script.js
+++ public/script.js
@@ -1,8 +1,44 @@
 const socket = io('/')
-
-socket.emit('join-room', ROOM_ID,10)
-
-
-socket.on('user-connected',userId=>{
-    console.log('user connected: '+userId)
+const videoGrid = document.getElementById('video-grid')
+const newPeer = new Peer(undefined,{
+    host: '/',
+    port: '3001'
 })
+const myVideo = document.createElement('video')
+
+myVideo.muted = true
+
+navigator.mediaDevices.getUserMedia({
+    video:true,
+    audio:true
+}).then(stream=>{
+    addVideoStream(myVideo,stream)
+    socket.on('user-connected', userId =>{
+        connectNewUser(userId,stream)
+    })
+})
+
+
+newPeer.on('open', id=>{
+    socket.emit('join-room', ROOM_ID,id)
+})
+
+function connectNewUser(userId,stream){
+    const call = newPeer.call(userId,stream)
+    const video = document.createElement('video')
+    call.on('stream',newUserVideoStream=>{
+        addVideoStream(video,newUserVideoStream)
+    })
+    call.on('close',()=>{
+        video.remove()
+    })
+}
+
+
+function addVideoStream(video,stream){
+    video.srcObject = stream
+    video.addEventListener('loadedmetadata',()=>{
+        video.play()
+    })
+    videoGrid.append(video)
+}
